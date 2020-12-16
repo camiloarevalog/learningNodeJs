c9da083c37201bd4bdc9957f9262b758da06d60a
Cristian Arevalo <cristian.arevalo@norsoft.com.co>
2020-12-16 14:40:03 -0500

    Se empieza aprendiendo NodeJs

---
+++ nodejs-curso/index.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+fs.writeFile('./texto.txt', 'linea uno', function (err) {
+    if (err) {
+        console.log(err);
+    }
+    console.log('Archivo creado');
+});
+
+console.log('AUltima linea');
+
+
